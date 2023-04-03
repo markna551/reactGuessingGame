143c9428bbd5c9df0f58d06ee7b748013c7d1002
DESKTOP-NT13LPL\ccs_win10_17 <markey.wong@gmail.com>
2023-04-04 01:48:22 +0700

    imprement guessing game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [guessingNumber, setGuessingNumber] = useState("");
+  const [correctGuessing, setCorrectGuessing] = useState("");
+  const [number, setNumber] = useState(1)
+  useEffect(() => {
+    setNumber(Math.floor(Math.random() * 100))
+  }, []);
+
+  function checkGuessing() {
+    console.log(number)
+    if (guessingNumber === number.toString()) {
+      setCorrectGuessing("Correct Guessing Number is " + number)
+    }
+    else {
+      if (guessingNumber > number.toString()) {
+        setCorrectGuessing("Too many")
+      }
+      else if(guessingNumber < number.toString()) {
+        setCorrectGuessing("Too less")
+      }
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>
+          Guessing Game
+        </h1>
+        <div className="guessing">
+          <h4> Input Your Number between 0-99 For Guessing</h4>
+          {correctGuessing && <h2> {correctGuessing} </h2>}
+          <form>
+              <input
+                type="text" 
+                value={guessingNumber}
+                onChange={(e) => setGuessingNumber(e.target.value)}
+              />
+          </form>
+          <button onClick={checkGuessing}> guess</button>
+        </div>
     </div>
+
   );
 }
 
